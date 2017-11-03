2fa35d9a17c63c0108b73f945cf6d8c128d81a86
LoggingUnit <andreserafimov@gmail.com>
2017-11-03 20:42:45 +0300

    OOP implemented
    All functions performed with prototype OOP style

--- scripts.js
+++ scripts.js
@@ -1,18 +1,30 @@
-var IdHashPrevious= 0;
+/**
+ * Main frontend class.
+ * Takes address of Guestbook backend server and id of HTML page where to place output data (comments etc.)
+ * @param {HTML} endpointAddress address of HTML backend server
+ * @param {id} mountPoint id of HTML page element to place output data there
+ * @returns Your mom
+ */
+function FrontendContent(endpointAddress, mountPoint) {
+  this.idHashPrevious = 0;
+  this.endpointAddress = endpointAddress;
+  this.mountPoint = mountPoint;
+}
 
 /**
  * Takes id of comment and deletes it from server with DELETE query
  * @param {id} currentComment comment Object from server response commentsArray
  * @returns none
  */
-var deleteComment = function(id) {
+FrontendContent.prototype.deleteComment = function(id) {
   var xhrDelete = new XMLHttpRequest();
+  var that = this;
   xhrDelete.addEventListener("readystatechange", function () {
     if (this.readyState === this.DONE) {
-      getAllComments();
+      that.getAllComments();
     }
   });
-  xhrDelete.open("DELETE", "http://5.79.97.34:8090/guestbook/" + id);
+  xhrDelete.open("DELETE", this.endpointAddress + "/" + id);
   xhrDelete.send();
 }
 
@@ -21,14 +33,14 @@ var deleteComment = function(id) {
  * @param {Array} commentsArray Array of comments from backend CRUD service
  * @returns Resulting html
  */
-var refreshView = function(commentsArray) {
+FrontendContent.prototype.refreshView = function(commentsArray) {
   var result = '';
   console.log("refreshed");
   for (var i = 0; i < commentsArray.length; i++) {
     var currentComment = commentsArray[i];
     // Append html elements
     result += '<div class = "delete">';
-    result += '<input type="button" onclick=" deleteComment(' + currentComment.id+')" value="[X]"></input>';
+    result += '<input type="button" onclick="frontendContent.deleteComment(' + currentComment.id+')" value="[X]"></input>';
     result += '</div>';
     result += '<div class = "comment">';
     result += '<h3>' + currentComment.header + '</h3>';
@@ -36,37 +48,40 @@ var refreshView = function(commentsArray) {
     result += '</div>';
   }
   // Put result on page`
-  document.getElementById('comments').innerHTML = result;
+  document.getElementById(this.mountPoint).innerHTML = result;
   return result;
 }
 
 /**
  * Query server for all comments
  */
-var getAllComments = function() {
+FrontendContent.prototype.getAllComments = function() {
   var xhr = new XMLHttpRequest();
+  var that = this;
   xhr.addEventListener("readystatechange", function () {
     if (xhr.readyState === xhr.DONE) {
       var responseArray= JSON.parse(xhr.responseText);
-      var idHash = 0;
+      var hash = 0;
       for (var i = 0; i < responseArray.length; i++) {
-        idHash += responseArray[i].id;
+        hash += responseArray[i].id;
       }
-      console.log(idHash);
-      if (idHash !== IdHashPrevious) {
-        refreshView(responseArray);
-        IdHashPrevious = idHash;
+      console.log(hash);
+      if (hash !== that.idHashPrevious) {
+        that.refreshView(responseArray);
+        that.idHashPrevious = hash;
       }
     }
   })
-  xhr.open("GET", "http://5.79.97.34:8090/guestbook");
+  xhr.open("GET", this.endpointAddress);
   xhr.send();
 }
 
 /**
  * Takes text from "formHeader" and "formBody" elements of page joined them into string dataOutput and send by POST query
  */
-var postComment = function() {
+FrontendContent.prototype.postComment = function() {
+  console.log(this.endpointAddress);
+  var that = this;
   var headerElement = document.getElementById("formHeader");
   var bodyElement = document.getElementById("formBody");
   if ((headerElement.value !== "")&&(bodyElement.value !== ""))
@@ -76,20 +91,23 @@ var postComment = function() {
     var xhrPostForm = new XMLHttpRequest();
     xhrPostForm.addEventListener("readystatechange", function () {
       if (xhrPostForm.readyState === xhrPostForm.DONE) {
-        getAllComments();
+        //that.getAllComments();
       }
     })
-    xhrPostForm.open("POST", "http://5.79.97.34:8090/guestbook");
+    xhrPostForm.open("POST", this.endpointAddress);
     xhrPostForm.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=utf8");
     xhrPostForm.send(dataOutput);
   }
   else alert("Some field looks empty, please enter something");
 }
-var postButton = document.getElementById("buttonPost");
-postButton.addEventListener("click", postComment);
 
-getAllComments();
+var frontendContent = new FrontendContent("http://5.79.97.34:8090/guestbook","comments");
+
+var postButton = document.getElementById("buttonPost");
+postButton.addEventListener("click", frontendContent.postComment.bind(frontendContent));
+
+frontendContent.getAllComments();
 
 setInterval(function(){
-  getAllComments();
+  frontendContent.getAllComments();
 }, 5000);
