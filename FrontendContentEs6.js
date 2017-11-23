40a8b5f21ee8748331db512ec4577b273c78b822
LoggingUnit <andreserafimov@gmail.com>
2017-11-23 20:29:08 +0500

    Modal window implemented
    Modal window implemented instead of alert window in the case of POST button pressed with empty header or/and body fields

--- FrontendContentEs6.js
+++ FrontendContentEs6.js
@@ -39,7 +39,8 @@ class FrontendContent {
     refreshView(commentsArray) {
         var result = '';
         var mount = document.getElementById(this.mountPoint);
-        // Removes all child nodes (already uploaded comments in mount point)
+        // Removes all child nodes (removes all already uploaded comments in mount point)
+        // maybe it is not perfect idea all the time delete and create agaim comment even if they did not changed
         while (mount.firstChild) {
             console.log(mount.firstChild, "removed");
             mount.removeChild(mount.firstChild);
@@ -153,6 +154,19 @@ class FrontendContent {
             xhrPostForm.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=utf8");
             xhrPostForm.send(dataOutput);
         }
-        else alert("Some field looks empty, please enter something");
+        else {
+            // When the user clicks the button, open the modal
+            modal.style.display = "block";
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        };
     }
 }
--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@
 var frontendContent = new FrontendContent("http://5.79.97.34:8090/guestbook","comments");
 
 var postButton = document.getElementById("buttonPost");
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+
 postButton.addEventListener("click", frontendContent.postComment.bind(frontendContent));
 
 frontendContent.getAllComments();
