bc7135e9191f4b1c233255b3c77051283f1d7bc5
LoggingUnit <andreserafimov@gmail.com>
2017-12-07 19:32:58 +0500

    Minor HTML changes
    Visible area width label added to see display size

--- scripts.js
+++ scripts.js
@@ -5,12 +5,15 @@ var frontendContent = new FrontendContent("http://5.79.97.34:8090/guestbook","co
 var postButton = document.getElementById("buttonPost");
 
 var modal = document.getElementById('myModal');
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0];
 
-postButton.addEventListener("click", frontendContent.postComment.bind(frontendContent));
+var labelSize = document.getElementById('size');
+
+postButton.addEventListener('click', frontendContent.postComment.bind(frontendContent));
 
 frontendContent.getAllComments();
 
 setInterval(function(){
   frontendContent.getAllComments();
+  labelSize.innerText = "Visible area width: " + labelSize.offsetWidth + ' px';
 }, 5000);
